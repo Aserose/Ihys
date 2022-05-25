71acde37687775d6f6ec136d0c7b337299b1c275
Aserose <shishi-rendan@yandex.ru>
2022-05-25 15:00:50 +0300

    modify .env and .yml file reading

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,6 +4,8 @@ import (
 	"IhysBestowal/pkg/customLogger"
 	"github.com/ilyakaznacheev/cleanenv"
 	"github.com/joho/godotenv"
+	"path/filepath"
+	"runtime"
 )
 
 type Config struct {
@@ -14,7 +16,7 @@ type Config struct {
 }
 
 func NewCfg(log customLogger.Logger) (cfg Config) {
-	err := godotenv.Load(); if err != nil {
+	err := godotenv.Load(getEnvPath()); if err != nil {
 		log.Panic(log.CallInfoStr(), err.Error())
 	}
 
@@ -22,11 +24,29 @@ func NewCfg(log customLogger.Logger) (cfg Config) {
 		log.Panic(log.CallInfoStr(), err.Error())
 	}
 
-	err = cleanenv.ReadConfig("./internal/config/config.yml", &cfg); if err != nil {
+	err = cleanenv.ReadConfig(getYmlPath(), &cfg); if err != nil {
 		log.Panic(log.CallInfoStr(), err.Error())
 	}
 
 	cfg.Service.Telegram.WebhookLink = cfg.Handler.Host + cfg.Handler.Api.Telegram
 
 	return
+}
+
+func getYmlPath() string {
+	return filepath.Join(getDirectory(getFilename(), 0), "config.yml")
+}
+
+func getEnvPath() string {
+	return filepath.Join(getDirectory(getFilename(), 2), ".env")
+}
+
+func getDirectory(filename string, depth int) string {
+	if depth != 0 { return getDirectory(filepath.Dir(filename), depth-1) }
+	return filepath.Dir(filename)
+}
+
+func getFilename() string {
+	_, filename, _, _ := runtime.Caller(0)
+	return filename
 }
