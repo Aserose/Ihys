880c1730c9d9181b914fe18197ae44aaca907427
Aserose <shishi-rendan@yandex.ru>
2022-05-26 14:52:50 +0300

    edit repository test

--- internal/repository/repository_test.go
+++ internal/repository/repository_test.go
@@ -10,24 +10,49 @@ import (
 
 func TestRepository(t *testing.T) {
 	log := customLogger.NewLogger()
+	rep := newRepo(log)
+	key := "keykeykey"
+	user := dto.TGUser{
+		int64(11111),
+		int64(22222),
+	}
 
 	convey.Convey("init", t, func() {
-		user := dto.TGUser{
-			int64(11111),
-			int64(22222),
-		}
-		key := "f2132f23"
-		rep := NewRepository(log, config.NewCfg(log).Repository)
 
-		convey.Convey("put", func() {
-			defer rep.Vk().DeleteKey(user)
-
-			rep.Vk().PutKey(user, key)
-
-			convey.So(rep.Vk().GetKey(user), convey.ShouldEqual, key)
-
+		convey.Convey("auth", func() {
+			rep.testAuth(user, key)
 		})
 
 	})
 
+}
+
+type repo struct {
+	rep Repository
+	log customLogger.Logger
+}
+
+func newRepo(log customLogger.Logger) repo {
+	return repo{
+		rep: NewRepository(log, config.NewCfg(log).Repository),
+		log: log,
+	}
+}
+
+func (r repo) testAuth(user dto.TGUser, key string) {
+	defer r.clean(user)
+
+	r.testAuthVk(user, key)
+
+}
+
+func (r repo) clean(user dto.TGUser) {
+	r.rep.Auth.Vk().DeleteKey(user)
+}
+
+func (r repo) testAuthVk(user dto.TGUser, key string) {
+	authVk := r.rep.Auth.Vk()
+
+	authVk.PutKey(user, key)
+	convey.So(authVk.GetKey(user), convey.ShouldEqual, key)
 }
