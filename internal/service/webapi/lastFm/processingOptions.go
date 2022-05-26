907a4d927c24c10a0a045d78ea7ef32a4651b32d
Aserose <shishi-rendan@yandex.ru>
2022-05-26 14:55:51 +0300

    option notes added; maxNumberOfSongs removed

--- internal/service/webapi/lastFm/processingOptions.go
+++ internal/service/webapi/lastFm/processingOptions.go
@@ -11,36 +11,37 @@ type options struct {
 
 type processingOptions func(e *options)
 
+// Parameter to set the max number of similar songs per item source.
+// Example: if the items source has two songs, a value of 3 will result in 6 songs in the response.
 func setMaxAudioAmountPerSource(num int) processingOptions {
 	return func(e *options) {
 		e.maxAudioAmountPerSource = num
 	}
 }
 
+// Parameter to set the number of songs from an artist.
+// Example: when set to 2, the response will contain 2 songs from the same artist.
 func setMaxAudioAmountPerArtist(num int) processingOptions {
 	return func(e *options) {
 		e.maxAudioAmountPerArtist = num
 	}
 }
 
+// Parameter to set the number of items handled by a single thread that were retrieved by the search.
 func setQuantityFlow(num int) processingOptions {
 	return func(e *options) {
 		e.quantityFlow = num
 	}
 }
 
-func setNumberOfSong(num int) processingOptions {
-	return func(e *options) {
-		e.maxNumberOfSong = num
-	}
-}
-
-func maxNumSimiliarArtists(num int) processingOptions {
+// Parameter to limit the number when requesting similar artists.
+func setMaxNumSimiliarArtists(num int) processingOptions {
 	return func(e *options) {
 		e.maxNumSimiliarArtists = num
 	}
 }
-func maxNumTopPerArtist(num int) processingOptions {
+// Parameter to limit the number of top songs from an artist.
+func setMaxNumTopPerArtist(num int) processingOptions {
 	return func(e *options) {
 		e.maxNumTopPerArtist = num
 	}
