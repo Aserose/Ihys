c48ee7854288c77330146f2eb03aa29530736403
Aserose <shishi-rendan@yandex.ru>
2022-05-27 14:10:06 +0300

    revised yaMusic processing options; add notes

--- internal/service/webapi/yaMusic/processingOptions.go
+++ internal/service/webapi/yaMusic/processingOptions.go
@@ -2,33 +2,31 @@ package yaMusic
 
 type options struct {
 	quantityFlow         int
-	numberOfSong         int
-	audioAmountPerSource int
-	audioAmountPerArtist int
+	maxAudioAmountPerSource int
+	maxAudioAmountPerArtist int
 }
 
 type processingOptions func(e *options)
 
-func setAudioAmountPerSource(num int) processingOptions {
+// Parameter to set the max number of similar songs per item source.
+// Example: if the items source has two songs, a value of 3 will result in 6 songs in the response.
+func setMaxAudioAmountPerSource(num int) processingOptions {
 	return func(e *options) {
-		e.audioAmountPerSource = num
+		e.maxAudioAmountPerSource = num
 	}
 }
 
-func setAudioAmountPerArtist(num int) processingOptions {
+// Parameter to set the number of songs from an artist.
+// Example: when set to 2, the response will contain 2 songs from the same artist.
+func setMaxAudioAmountPerArtist(num int) processingOptions {
 	return func(e *options) {
-		e.audioAmountPerArtist = num
+		e.maxAudioAmountPerArtist = num
 	}
 }
 
+// Parameter to set the number of items handled by a single thread that were retrieved by the search.
 func setQuantityFlow(num int) processingOptions {
 	return func(e *options) {
 		e.quantityFlow = num
-	}
-}
-
-func setNumberOfSong(num int) processingOptions {
-	return func(e *options) {
-		e.numberOfSong = num
 	}
 }
