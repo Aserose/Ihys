8f052f5e760f632bc7cc540f63959243440589a8
Aserose <shishi-rendan@yandex.ru>
2022-05-27 13:36:53 +0300

    fix test

--- internal/service/webapi/lastFm/collater_test.go
+++ internal/service/webapi/lastFm/collater_test.go
@@ -19,18 +19,46 @@ func TestCollater(t *testing.T) {
 
 	convey.Convey("init", t, func() {
 
-		convey.Convey("MaxAudioAmountPerSource", func() {
-			maxAudioAmountPerSource := 4
-			convey.So(
-				len(newCollater(enq, setMaxAudioAmountPerSource(maxAudioAmountPerSource)).getSimiliars(userId, sourceItems).Items),
-				convey.ShouldBeLessThanOrEqualTo,
-				maxAudioAmountPerSource*len(sourceItems.Items))
+		convey.Convey("option <MaxAudioAmountPerSource>", func() {
+			getSimiliar := func(maxAudioAmountPerSource int) {
+				equalValue := maxAudioAmountPerSource*len(sourceItems.Items)
+				assertion := convey.ShouldEqual
+				if maxAudioAmountPerSource < 0 { equalValue = 0 }
+				if maxAudioAmountPerSource > 20 { assertion = convey.ShouldBeLessThanOrEqualTo }
+
+				convey.So(
+					len(newCollater(enq, setMaxAudioAmountPerSource(maxAudioAmountPerSource)).getSimiliars(userId, sourceItems).Items),
+					assertion, equalValue)
+			}
+
+			for _, num := range []int{ 4, 74, 0 -4 } {
+				getSimiliar(num)
+			}
+
 		})
 
-		convey.Convey("MaxAudioAmountPerArtist", func() {
-			listArtists := getAListOfArtists(newCollater(enq, setMaxAudioAmountPerArtist(4)).getSimiliars(userId, sourceItems).Items)
-			sort.Strings(listArtists)
-			convey.So(listArtists[0], convey.ShouldEqual, listArtists[1])
+		convey.Convey("option <MaxAudioAmountPerArtist>", func() {
+			artistAlreadyOnTheList := func(listArtist []string) bool {
+				counter := make(map[string]int)
+				for _, artist := range listArtist {
+					counter[artist]++
+					if counter[artist] > 1 { return true }
+				}
+				return false
+			}
+
+			testFunc := func(maxAudioAmountPerSource int) {
+				equalValue := false
+				if maxAudioAmountPerSource > 1 { equalValue = true }
+
+				listArtists := getAListOfArtists(newCollater(enq, setMaxAudioAmountPerArtist(maxAudioAmountPerSource)).getSimiliars(userId, sourceItems).Items)
+				sort.Strings(listArtists)
+				convey.So(artistAlreadyOnTheList(listArtists), convey.ShouldEqual, equalValue)
+			}
+
+			for _, num := range []int{ 1, 4 } {
+				testFunc(num)
+			}
 		})
 	})
 }
